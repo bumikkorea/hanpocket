476049bb05650aee97ddd400a3cab5d36825d141
구월 <guwol@hanpocket.kr>
2026-02-23 22:20:05 +0900

    홈 카드 높이 200px

--- visa-app/src/components/HomeTab.jsx
+++ visa-app/src/components/HomeTab.jsx
@@ -245,7 +245,7 @@ export default function HomeTab({ profile, lang, exchangeRate, setTab }) {
       <div
         ref={containerRef}
         className="relative overflow-hidden mx-4"
-        style={{ height: '250px', marginTop: '8px' }}
+        style={{ height: '200px', marginTop: '8px' }}
         onTouchStart={onTouchStart}
         onTouchMove={onTouchMove}
         onTouchEnd={onTouchEnd}
